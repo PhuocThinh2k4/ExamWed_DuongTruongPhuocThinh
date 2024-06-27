0c74cfa085a858e52001adbc3c91cebd2a35c987
ADMIN <ADMIN@PC236>
2024-06-27 09:32:30 +0700

    Add project files.

---
+++ ExamWed_DuongTruongPhuocThinh/Migrations/20240627011434_SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamWed_DuongTruongPhuocThinh.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaNhacs",
+                columns: new[] { "Id", "NgheSi", "TheLoai", "SoLuong", "GiaBan", "TuaDe" },
+                values: new object[,]
+                {
+                    { 1, "A", 105000.0, 110, true, "CD Nỗi buồn gác trọ" },
+                    { 2, "V", 250000.0, 161, false, "CD Bài tình ca đêm" },
+                    { 3, "R", 230000.0, 105, true, "CD BABYMONSTER" },
+                    { 4, "B", 150000.0, 150, true, "CD Chuyện chúng mình" },
+                    { 5, "T", 27000.0, 120, true, "CD Today's US-UK Hit" },
+                    { 6, "G", 220000.0, 107, false, "CD Đoá hồng nhạc Việt" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "DiaNhacs",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
---
+++ ExamWed_DuongTruongPhuocThinh/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExamWed_DuongTruongPhuocThinh
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> ops) : base(ops)
+        {
+        }
+        public DbSet<ExamDB> ExamDB { set; get; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ExamDB>().HasData(
+            new ExamDB { Id = 1, Title = " CD Nỗi buồn gác trọ", Author = "Việt Nam", Price = 105.000, Quantity = 20 },
+            new ExamDB { Id = 2, Title = "CD Bài tình ca đêm", Author = "Việt Nam", Price = 250.000, Quantity = 15 },
+            new ExamDB { Id = 3, Title = "CD BABYMONSTER", Author = "Nước Ngoài", Price = 230.000, Quantity = 10 },
+            new ExamDB { Id = 4, Title = " CD Chuyện chúng mình", Author = "Việt Nam", Price = 150.000, Quantity = 30 },
+            new ExamDB { Id = 5, Title = "CD Today's US-UK Hit", Author = "Nước Ngoài", Price = 27.000, Quantity = 10 },
+            new ExamDB { Id = 6, Title = "CD Đoá hồng nhạc Việt", Author = "Việt Nam", Price = 220.000, Quantity = 40 }
+            );
+        }
+    }
+}
